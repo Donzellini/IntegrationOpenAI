97416dd2b6faac81031f74d31915f2a24049878c
Muriel Donzellini <muriel.donzellini@gmail.com>
2024-07-14 17:06:51 -0300

    lint

--- app/main/openai/openai_response_models.py
+++ app/main/openai/openai_response_models.py
@@ -1,13 +1,11 @@
 from flask_restx import Namespace, fields
 
-api = Namespace("E-commerce OpenAI", description="API de integração com OpenAI para assistência de vendas.")
+api = Namespace(
+    "E-commerce OpenAI", description="API de integração com OpenAI para assistência de vendas."
+)
 
 
 class ResponseQuestionAnswer:
-    question_post_model = api.model(
-        "QuestionModel", {"question": fields.String}
-    )
+    question_post_model = api.model("QuestionModel", {"question": fields.String})
 
-    question_post_response_model = api.model(
-        "QuestionResponseModel", {"response": fields.String}
-    )
+    question_post_response_model = api.model("QuestionResponseModel", {"response": fields.String})
